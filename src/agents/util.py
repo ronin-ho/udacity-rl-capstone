aa0343dd9e8299d1f2c452737477fbcc6e411244
Ronin Ho <rhktechdev@gmail.com>
2019-07-07 15:48:36 +0800

    Feat: Update main function and add utility for Agents execution

---
+++ src/agents/util.py
@@ -0,0 +1,169 @@
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras.initializers import RandomUniform, Initializer, Constant
+import numpy as np
+import copy
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state
+
+#https://stable-baselines.readthedocs.io/en/master/_modules/stable_baselines/ddpg/noise.html#NormalActionNoise
+class NormalActionNoise:
+    """
+    A gaussian action noise
+
+    :param mean: (float) the mean value of the noise
+    :param sigma: (float) the scale of the noise (std here)
+    """
+    def __init__(self, size, mean, sigma):
+        self._mu = mean * np.ones(size)
+        self._sigma = sigma
+
+    def sample(self):
+        return np.random.normal(self._mu, self._sigma)
+
+    def reset(self):
+        return
+    
+# Based on https://github.com/openai/baselines/blob/master/baselines/common/mpi_running_mean_std.py
+class WhiteningNormalizer(object):
+    def __init__(self, shape, eps=1e-2, dtype=np.float64):
+        self.eps = eps
+        self.shape = shape
+        self.dtype = dtype
+
+        self._sum = np.zeros(shape, dtype=dtype)
+        self._sumsq = np.zeros(shape, dtype=dtype)
+        self._count = 0
+
+        self.mean = np.zeros(shape, dtype=dtype)
+        self.std = np.ones(shape, dtype=dtype)
+
+    def normalize(self, x):
+        return (x - self.mean) / self.std
+
+    def denormalize(self, x):
+        return self.std * x + self.mean
+
+    def update(self, x):
+        if x.ndim == len(self.shape):
+            x = x.reshape(-1, *self.shape)
+        assert x.shape[1:] == self.shape
+
+        self._count += x.shape[0]
+        self._sum += np.sum(x, axis=0)
+        self._sumsq += np.sum(np.square(x), axis=0)
+
+        self.mean = self._sum / float(self._count)
+        self.std = np.sqrt(np.maximum(np.square(self.eps), self._sumsq / float(self._count) - np.square(self.mean)))
+
+#https://github.com/PetraVidnerova/rbf_keras/blob/master/rbflayer.py
+class InitCentersRandom(Initializer):
+    """ Initializer for initialization of centers of RBF network
+        as random samples from the given data set.
+
+    # Arguments
+        X: matrix, dataset to choose the centers from (random rows
+          are taken as centers)
+    """
+
+    def __init__(self, X):
+        self.X = X
+
+    def __call__(self, shape, dtype=None):
+        assert shape[1] == self.X.shape[1]
+        idx = np.random.randint(self.X.shape[0], size=shape[0])
+        return self.X[idx, :]
+
+
+class RBFLayer(Layer):
+    """ Layer of Gaussian RBF units.
+
+    # Example
+
+    ```python
+        model = Sequential()
+        model.add(RBFLayer(10,
+                           initializer=InitCentersRandom(X),
+                           betas=1.0,
+                           input_shape=(1,)))
+        model.add(Dense(1))
+    ```
+
+
+    # Arguments
+        output_dim: number of hidden units (i.e. number of outputs of the
+                    layer)
+        initializer: instance of initiliazer to initialize centers
+        betas: float, initial value for betas
+
+    """
+
+    def __init__(self, output_dim, initializer=None, betas=1.0, **kwargs):
+        self.output_dim = output_dim
+        self.init_betas = betas
+        if not initializer:
+            self.initializer = RandomUniform(0.0, 1.0)
+        else:
+            self.initializer = initializer
+        super(RBFLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        self.centers = self.add_weight(name='centers',
+                                       shape=(self.output_dim, input_shape[1]),
+                                       initializer=self.initializer,
+                                       trainable=True)
+        self.betas = self.add_weight(name='betas',
+                                     shape=(self.output_dim,),
+                                     initializer=Constant(
+                                         value=self.init_betas),
+                                     # initializer='ones',
+                                     trainable=True)
+
+        super(RBFLayer, self).build(input_shape)
+
+    def call(self, x):
+
+        C = K.expand_dims(self.centers)
+        H = K.transpose(C-K.transpose(x))
+        return K.exp(-self.betas * K.sum(H**2, axis=1))
+
+        # C = self.centers[np.newaxis, :, :]
+        # X = x[:, np.newaxis, :]
+
+        # diffnorm = K.sum((C-X)**2, axis=-1)
+        # ret = K.exp( - self.betas * diffnorm)
+        # return ret
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+    def get_config(self):
+        # have to define get_config to be able to use model_from_json
+        config = {
+            'output_dim': self.output_dim
+        }
+        base_config = super(RBFLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+  
+        
--- src/main.py
+++ src/main.py
@@ -1,61 +1,49 @@
 import argparse
 import sys
 import os
-# import os.path
 from time import gmtime, strftime
 
 import gym
-from gym import wrappers#, logger
+from gym import wrappers
 import csv
 import logging
+from copy import deepcopy
+
+import numpy as np
 
 from agents.RandomAgent import RandomAgent
 from agents.DDPG import DDPG
-from agents.DDPG2 import DDPG2
+from agents.RBFAgent import RBFAgent
 
-def run(agent_id, episode_count, start_episode, load_dir):    
-
-    # You can set the level to logger.DEBUG or logger.WARN if you
-    # want to change the amount of output.
-    #logger.setLevel(logger.INFO)
-
+def run(agent_id, is_training, load_dir):    
     env = gym.make('LunarLanderContinuous-v2')
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
     time_format = strftime("%Y%m%d%H%M%S", gmtime())
-    outdir = './results/' + time_format+ '-' + agent_id + '-' + str(start_episode) + '-' + str(start_episode + episode_count -1) +'/'
-    #env = wrappers.Monitor(env, directory=outdir, force=True)
-    env.seed(0)
-    #agent = RandomAgent(env.action_space)
+    outdir = './results/' + time_format+ '-' + agent_id +'/'
     
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+    if not os.path.exists(outdir + 'video/'):
+        os.makedirs(outdir + 'video/')
+    
+    #Enable below comment to record video
+    #env = wrappers.Monitor(env, directory=outdir + 'video/', force=True)
+    env.seed(123)
+    np.random.seed(123)
 
     #set up logger    
-#    logger = logging.getLogger('gym')
-#    fh = logging.FileHandler(outdir + 'application.log')
-#    fh.setLevel(logging.INFO)
-#    logger.addHandler(fh)
     logging.basicConfig(filename=outdir + 'application.log',level=logging.INFO)
     logger = logging.getLogger('gym')    
     
     agent = None
     if(agent_id == 'RandomAgent'):
-        agent = RandomAgent(env.action_space)
+        agent = RandomAgent(env)
     elif(agent_id == 'DDPG'):
         agent = DDPG(env)
-    elif(agent_id == 'DDPG2'):
-        agent = DDPG2(env)
+        agent.build_models()
+    elif(agent_id == 'RBFAgent'):
+        agent = RBFAgent(env)
     else:
         logger.error("Invalid Agent chosen!")
         return
-
-    #episode_count = 2000
-    score = 0
-    done = False
 
     if load_dir is not None:
         logger.info("Load model at " + load_dir)
@@ -66,46 +54,73 @@ def run(agent_id, episode_count, start_episode, load_dir):
     with open(file_output, 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(labels)
-    #
-        for i in range(start_episode, start_episode + episode_count):
-            ob = env.reset()
-            agent.reset_episode(ob)
-            score = 0
-            logger.info("Start episode " + str(i))
-            while True:
-                action = agent.act(ob)
+
+        episode = np.int16(1)
+        step = np.int16(0)
+        observation = None
+        episode_reward = None
+        max_steps = 1000000
+        max_episodes = 1100 + episode
+        
+        try:
+            while step < max_steps and episode < max_episodes:
                 
-#                 reward = 0
-#                 for _ in range(3):
-#                     ob, tmp_reward, done, _ = env.step(action)
-#                     reward += tmp_reward
-#                     if done:
-#                         break
-                ob, reward, done, _ = env.step(action)
+                if observation is None:  # start of a new episode
+                    logger.info("Start episode " + str(episode))
+                    episode_reward = np.float32(0)
+
+                    # Obtain the initial observation by resetting the environment.
+                    agent.reset_episode()
+                    observation = deepcopy(env.reset())
+
+                action = agent.act(observation)
                     
-                agent.step(action, reward, ob, done)
-                score += reward
+                reward = np.float32(0)
+                done = False
+                
+                next_state, r, done, info = env.step(action)
+                next_state = deepcopy(next_state)
+                reward = r
+                
+                if is_training:
+                    agent.step(observation, action, reward, next_state, done)
+                    
+                episode_reward += reward
+                step += 1
+                
+                observation = next_state
+                
+                if episode % 20 ==0:
+                    logger.info("State-Action: {}, {}, {}, {}, reward: {}".format(observation[0], observation[1], action[0],action[1], reward))
+                    
                 if done:
-                    to_write = [i] + [score]
+                    # Act on the final state
+                    # Step on final state but without adding to memory as next state is the reset state
+                    action = agent.act(next_state)
+                    agent.step_without_memory()
+
+                    to_write = [episode] + [episode_reward]
                     writer.writerow(to_write)
-                    if i % 50 ==0:
+                    if episode % 20 ==0:
                         csvfile.flush()
-                    if i % 100 == 0:
+                    if episode % 20 == 0:
                         agent.save_weight(outdir)
                         
-                    break
-                # Note there's no env.render() here. But the environment still can open window and
-                # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-                # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
-        # Close the env and write monitor result info to disk
+                    episode += 1
+                    observation = None
+                    episode_reward = None    
+                    
+        except KeyboardInterrupt:
+            csvfile.flush()
+            agent.save_weight(outdir)
+            
+        # Close the env
         env.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-a', dest='agent_id', nargs='?', help='Select a agent to run',required=True)
-    parser.add_argument('-e', dest='episode_count', nargs='?', help='episode count', type=int, default=1000)
-    parser.add_argument('-s', dest='start_episode', nargs='?', help='episode start', type=int, default=1)
-    parser.add_argument('-l', dest='load_dir', nargs='?', help='load model directory')
+    parser.add_argument('-t', dest='is_training', nargs='?', help='1 for training, 0 for testing', type=int, default=1)
+    parser.add_argument('-l', dest='load_dir', nargs='?', help='Path to load model or weight')
     args = parser.parse_args()
-    run(args.agent_id, args.episode_count, args.start_episode, args.load_dir)
+    run(args.agent_id, args.is_training, args.load_dir)
